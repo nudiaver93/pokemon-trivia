2e5c8c3b4b73529befa9feb27c5b9aa0c64cd023
Navin Udiaver <navinudiaver@navins-mbp.attlocal.net>
2016-11-02 03:59:56 -0500

    finished game object and media queries

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+
 var questions = [{
   question: 'Misty is the Gym Leader for which city?',
   answers: ["Saffron City", "Celadon City", "Cerulean City", "Lavender Town"],
@@ -14,7 +15,7 @@ var questions = [{
   correctAnswer: "Silph Co",
   image:"assets/images/lapras.gif"
 }, {
-  question: 'In Generation 1, the Pokemon with the highest attack stat is ___',
+  question: 'In Generation 1, the Pokemon with the highest attack stat is _',
   answers: ["Tyranitar", "Machamp", "Mewtwo", "Dragonite"],
   correctAnswer: "Dragonite",
   image:"assets/images/dragonite.gif"
@@ -29,7 +30,7 @@ var questions = [{
   correctAnswer: "Seaking",
   image:"assets/images/seaking.gif"
 }, {
-  question: "__ is the name of the glitched Pokemon in Pokémon Red.",
+  question: "_ is the name of the glitched Pokemon in Pokemon Red.",
   answers: ["Mewthree", "UB-01", "Missingno", "Shuckle"],
   correctAnswer: "Missingno",
   image:"assets/images/missingno.gif"
@@ -48,7 +49,7 @@ var game = {
   counter:timerCount,
   correct:0,
   incorrect:0,
-
+  
   countdown: function(){
     game.counter--;
   
@@ -66,22 +67,23 @@ var game = {
   
     $('.content').fadeOut('slow');
   
-    var questionhtml = '<h2>Time Left: <span id="counter-number">30</span> Seconds</h2><h2>' + 
+    var questionhtml = '<h2>Timer: <span id="counter-number">30</span></h2><h2>' + 
         (game.currentQuestion + 1) + '. ' + questions[this.currentQuestion].question + '</h2>';
         for (var i = 0; i<questions[this.currentQuestion].answers.length; i++){
           questionhtml += '<button class="answer-button" id="button"' + 
           'data-name="' + questions[this.currentQuestion].answers[i] + '">' + 
           questions[this.currentQuestion].answers[i]+ '</button>';
         };
-  
+
     $('.content').queue(function (next) {
       $(this).empty();
       $(this).append(questionhtml);
       $(this).fadeIn('slow');
-    next();
-  });
-  
+      next();
+    });
+    
   },
+  
 
   nextQuestion: function(){
     game.counter = timerCount;
@@ -91,19 +93,17 @@ var game = {
     game.currentQuestion++;
   
     game.loadQuestion();
+  
   },
-
+  
 
   timeUp: function (){
     clearInterval(timer);
 
     $('.content').fadeOut('slow');
 
-    var correctAnswer = questions[this.currentQuestion].correctAnswer;
-    var image = question[this.currentQuestion].image;
-
-    var resulthtml = '<h2>Out of Time! It was ' + correctAnswer + 
-                      '!</h2>' + '<img src="' + image + '" loop="infinite"/>';
+    var resulthtml = '<h2>Out of Time! It was ' + questions[this.currentQuestion].correctAnswer + 
+                      '!</h2>' + '<img src="' + questions[this.currentQuestion].image + '"/>';
 
     $('.content').queue(function (next) {
       $(this).empty();
@@ -118,8 +118,99 @@ var game = {
       setTimeout(game.nextQuestion, 3000);
     }
   },
-}
 
+
+  results: function() {
+    clearInterval(timer);
+
+    $('.content').fadeOut('slow');
+
+    var resultshtml = '<h2>The results are in!</h2><h3>' + 
+        'Correct Answers: ' + game.correct + '</h3><h3>Incorrect Answers: ' + 
+        game.incorrect + '</h3><h3>Unanswered: ' + 
+        (questions.length - (game.incorrect + game.correct)) + '</h3>' +
+        '<br><button id="start-over">Start Over?</button>';
+
+    $('.content').queue(function (next) {
+      $(this).empty();
+      $(this).append(resultshtml);
+      $(this).fadeIn('slow');
+      next();
+    });
+  },
+
+
+  clicked: function(e) {
+    clearInterval(timer);
+
+    if ($(e.target).data("name") === questions[this.currentQuestion].correctAnswer){
+      this.answeredCorrectly();
+    } 
+    else {
+      this.answeredIncorrectly();
+    }
+  },
+
+
+  answeredIncorrectly: function() {
+    clearInterval(timer);
+    game.incorrect++;
+    
+    $('.content').fadeOut('slow');
+
+    var resulthtml = '<h2>Sorry! It was ' + questions[this.currentQuestion].correctAnswer + 
+                      '!</h2>' + '<img src="' + questions[this.currentQuestion].image + '"/>';
+
+    $('.content').queue(function (next) {
+      $(this).empty();
+      $(this).append(resulthtml);
+      $(this).fadeIn('fast');
+      next();
+    })
+
+    if (game.currentQuestion === questions.length - 1){
+      setTimeout(game.results, 3000);
+    } else {
+      setTimeout(game.nextQuestion, 3000);
+    }
+  },
+
+
+  answeredCorrectly: function(){
+    clearInterval(timer);
+    game.correct++;
+    $('.content').fadeOut('slow');
+
+    var resulthtml = '<h2>Correct! It was ' + questions[this.currentQuestion].correctAnswer + 
+                      '!</h2>' + '<img src="' + questions[this.currentQuestion].image + '"/>';
+
+    $('.content').queue(function (next) {
+      $(this).empty();
+      $(this).append(resulthtml);
+      $(this).fadeIn('fast');
+      next();
+    });
+
+    if (game.currentQuestion === questions.length - 1){
+      setTimeout(game.results, 3000);
+    } else {
+      setTimeout(game.nextQuestion, 3000);
+    }
+  },
+
+
+  reset: function(){
+    location.reload();
+  }
+};
+
+$(document).on('click', '#start-over', function(e) {
+  game.reset();
+});
+
+$(document).on('click', '.answer-button', function(e) {
+  game.clicked(e);
+});
 
 $(document).on('click', '#start', function(e) {
   game.loadQuestion();
