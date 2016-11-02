40ade97f154614f9332ae46f29bf1fe94aa2f1e9
Navin Udiaver <navinudiaver@navins-mbp.attlocal.net>
2016-11-02 01:58:38 -0500

    added most animations and question loading

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,9 @@
 var questions = [{
+  question: 'Misty is the Gym Leader for which city?',
+  answers: ["Saffron City", "Celadon City", "Cerulean City", "Lavender Town"],
+  correctAnswer: "Cerulean City",
+  image:"assets/images/misty.gif"
+}, {
   question: "Which stone can be used to evolve Eevee?",
   answers: ["Leaf Stone", "Moon Stone", "Fire Stone", "Sun Stone"],
   correctAnswer: "Fire Stone",
@@ -9,43 +14,72 @@ var questions = [{
   correctAnswer: "Silph Co",
   image:"assets/images/lapras.gif"
 }, {
+  question: 'In Generation 1, the Pokemon with the highest attack stat is ___',
+  answers: ["Tyranitar", "Machamp", "Mewtwo", "Dragonite"],
+  correctAnswer: "Dragonite",
+  image:"assets/images/dragonite.gif"
+}, {
   question: "Which Pokemon did Ash Ketchum release?",
   answers: ["Pikachu", "Tauros", "Snorlax", "Butterfree"],
   correctAnswer: "Butterfree",
   image:"assets/images/butterfree.gif"
 }, {
-  question: 'Sabrina is the Gym Leader for which city?',
-  answers: ["Saffron City", "Celadon City", "Cerulean City", "Lavender Town"],
-  correctAnswer: "Saffron City",
-  image:"assets/images/sabrina.gif"
-}, {
-  question: 'In Generation 1, the Pokémon with the highest attack stat is ___',
-  answers: ["Tyranitar", "Machamp", "Mewtwo", "Dragonite"],
-  correctAnswer: "Dragonite",
-  image:"assets/images/dragonite.gif"
-}, {
-  question: "Which of these Pokémon is not an ice type?",
+  question: "Which of these Pokemon is not an ice type?",
   answers: ["Shellder", "Seaking", "Dewgong", "Jynx"],
   correctAnswer: "Seaking",
   image:"assets/images/seaking.gif"
 }, {
-  question: "__ is the name of the glitched Pokémon in Pokémon Red.",
+  question: "__ is the name of the glitched Pokemon in Pokémon Red.",
   answers: ["Mewthree", "UB-01", "Missingno", "Shuckle"],
   correctAnswer: "Missingno",
   image:"assets/images/missingno.gif"
 }, {
-  question: "Which Pokémon types were added in Generation 2?",
-  answers: ["Dark and Ghost", "Dark and Steel", "Fairy and Steel", "Fairy and Ghost"],
-  correctAnswer: "Dark and Steel",
-  image:"assets/images/magneton.gif"
+  question: "Which Pokemon type was added in Generation 2?",
+  answers: ["Ghost", "Steel", "Fairy", "Gold"],
+  correctAnswer: "Steel",
+  image:"assets/images/magnemite.gif"
 }];
 
 var timerCount = 30;
 
 var game = {
   questions:questions,
-  currentQuestion: 1,
+  currentQuestion: 0,
   counter:timerCount,
   correct:0,
   incorrect:0,
+
+  countdown: function(){
+    game.counter--;
+  
+    $('#counter-number').html(game.counter);
+  
+    if (game.counter === 0){
+      game.timeUp();
+    }
+  
+  },
+  
+
+  loadQuestion: function(){
+    timer = setInterval(game.countdown, 1000);
+  
+    $('.content').fadeOut('slow');
+  
+    var questionhtml = '<h2>Time Left: <span id="counter-number">30</span> Seconds</h2><h2>' + 
+        (game.currentQuestion + 1) + '. ' + questions[this.currentQuestion].question + '</h2>';
+        for (var i = 0; i<questions[this.currentQuestion].answers.length; i++){
+          questionhtml += '<button class="answer-button" id="button"' + 
+          'data-name="' + questions[this.currentQuestion].answers[i] + '">' + 
+          questions[this.currentQuestion].answers[i]+ '</button>';
+        };
+  
+    $('.content').queue(function (next) {
+      $(this).empty();
+      $(this).append(questionhtml);
+      $(this).fadeIn('slow');
+    next();
+  });
+  
+  },
 }
