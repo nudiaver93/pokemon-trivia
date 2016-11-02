9c8b797096721423390dd00dcbc83953dd07d0e9
Navin Udiaver <navinudiaver@navins-mbp.attlocal.net>
2016-11-01 23:34:31 -0500

    questions and gifs

---
+++ assets/js/app.js
@@ -0,0 +1,41 @@
+var questions = [{
+  question: "Which stone can be used to evolve Eevee?",
+  answers: ["Leaf Stone", "Moon Stone", "Fire Stone", "Sun Stone"],
+  correctAnswer: "Fire Stone",
+  image:"assets/images/eevee.gif"
+}, {
+  question: "In Pokemon Red, where does the player catch Lapras?",
+  answers: ["Silph Co", "Safari Zone", "Cinnabar Island", "Diglett's Cave"],
+  correctAnswer: "Silph Co",
+  image:"assets/images/lapras.gif"
+}, {
+  question: "Which Pokemon did Ash Ketchum release?",
+  answers: ["Pikachu", "Tauros", "Snorlax", "Butterfree"],
+  correctAnswer: "Butterfree",
+  image:"assets/images/butterfree.gif"
+}, {
+  question: 'Sabrina is the Gym Leader for which city?',
+  answers: ["Saffron City", "Celadon City", "Cerulean City", "Lavender Town"],
+  correctAnswer: "Saffron City",
+  image:"assets/images/sabrina.gif"
+}, {
+  question: 'In Generation 1, the Pokémon with the highest attack stat is ___',
+  answers: ["Tyranitar", "Machamp", "Mewtwo", "Dragonite"],
+  correctAnswer: "Dragonite",
+  image:"assets/images/dragonite.gif"
+}, {
+  question: "Which of these Pokémon is not an ice type?",
+  answers: ["Shellder", "Seaking", "Dewgong", "Jynx"],
+  correctAnswer: "Seaking",
+  image:"assets/images/seaking.gif"
+}, {
+  question: "__ is the name of the glitched Pokémon in Pokémon Red.",
+  answers: ["Mewthree", "UB-01", "Missingno", "Shuckle"],
+  correctAnswer: "Missingno",
+  image:"assets/images/missingno.gif"
+}, {
+  question: "Which Pokémon types were added in Generation 2?",
+  answers: ["Dark and Ghost", "Dark and Steel", "Fairy and Steel", "Fairy and Ghost"],
+  correctAnswer: "Dark and Steel",
+  image:"assets/images/magneton.gif"
+}];a
